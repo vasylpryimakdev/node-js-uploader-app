3796590a3b589c97df712671c13ba02edeedcbbf
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-30 19:41:16 +0300

    feat: add dynamic file upload handling with backpressure support

--- client.js
+++ client.js
@@ -1,17 +1,27 @@
 const net = require("net");
 const fs = require("node:fs/promises");
+const path = require("path");
 
 const socket = net.createConnection({ host: "::1", port: 5050 }, async () => {
-  const filePath = "./text.txt";
+  const filePath = process.argv[2];
+  const fileName = path.basename(filePath);
   const fileHandle = await fs.open(filePath, "r");
-  const fileStream = fileHandle.createReadStream();
+  const fileReadStream = fileHandle.createReadStream(); // the stream to read from
+
+  socket.write(`fileName: ${fileName}-------`);
 
   // Reading from the source file
-  fileStream.on("data", (data) => {
-    socket.write(data);
+  fileReadStream.on("data", (data) => {
+    if (!socket.write(data)) {
+      fileReadStream.pause();
+    }
   });
 
-  fileStream.on("end", () => {
+  socket.on("drain", () => {
+    fileReadStream.resume();
+  });
+
+  fileReadStream.on("end", () => {
     console.log("The file was successfully uploaded!");
     socket.end();
   });
--- server.js
+++ server.js
@@ -9,16 +9,35 @@ server.on("connection", (socket) => {
   console.log("New connection!");
 
   socket.on("data", async (data) => {
-    fileHandle = await fs.open(`storage/test.txt`, "w");
-    fileWriteStream = fileHandle.createWriteStream();
+    if (!fileHandle) {
+      socket.pause(); // pause receiving data from the client
 
-    // Writing to our destination file
-    fileWriteStream.write(data);
+      const indexOfDivider = data.indexOf("-------");
+      const fileName = data.subarray(10, indexOfDivider).toString("utf-8");
+
+      fileHandle = await fs.open(`storage/${fileName}`, "w");
+      fileWriteStream = fileHandle.createWriteStream(); // the stream to write to
+
+      // Writing to our destination file, discard the headers
+      fileWriteStream.write(data.subarray(indexOfDivider + 7));
+
+      socket.resume(); // resume receiving data from the client
+      fileWriteStream.on("drain", () => {
+        socket.resume();
+      });
+    } else {
+      if (!fileWriteStream.write(data)) {
+        socket.pause();
+      }
+    }
   });
 
+  // This end event happens when the client.js file ends the socket
   socket.on("end", () => {
+    if (fileHandle) fileHandle.close();
+    fileHandle = undefined;
+    fileWriteStream = undefined;
     console.log("Connection ended!");
-    fileHandle.close();
   });
 });
 
